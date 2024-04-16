1fe6e40c79052395870248e2fb7169bc3b0fca27
staka99 <marko.staka12@gmail.com>
2024-04-16 14:05:24 +0200

    Dodavanje anotacije CrossOrigin

--- Backend/src/main/java/rvabackendg5/controller/DobavljacController.java
+++ Backend/src/main/java/rvabackendg5/controller/DobavljacController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import rvabackendg5.models.Dobavljac;
 import rvabackendg5.services.DobavljacService;
 
+@CrossOrigin
 @RestController
 public class DobavljacController {
 
--- Backend/src/main/java/rvabackendg5/controller/StavkaPorudzbineController.java
+++ Backend/src/main/java/rvabackendg5/controller/StavkaPorudzbineController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import rvabackendg5.services.ArtiklService;
 import rvabackendg5.services.PorudzbinaService;
 import rvabackendg5.services.StavkaPorudzbineService;
 
+@CrossOrigin
 @RestController
 public class StavkaPorudzbineController {
 
