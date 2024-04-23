a7d6ad5b52eea60c61705d3987bcae44a58da917
staka99 <marko.staka12@gmail.com>
2024-04-23 14:58:14 +0200

    Vezbe 8 - 23.04.2024. - Osnovne komponente

--- Frontend/g5-frontend/src/app/app.module.ts
+++ Frontend/g5-frontend/src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtiklComponent } from './main/artikl/artikl.component';
+import { DobavljacComponent } from './main/dobavljac/dobavljac.component';
+import { PorudzbinaComponent } from './main/porudzbina/porudzbina.component';
+import { StavkaPorudzbineComponent } from './main/stavka-porudzbine/stavka-porudzbine.component';
+import { HomeComponent } from './utility/home/home.component';
+import { AboutComponent } from './utility/about/about.component';
+import { AuthorComponent } from './utility/author/author.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtiklComponent,
+    DobavljacComponent,
+    PorudzbinaComponent,
+    StavkaPorudzbineComponent,
+    HomeComponent,
+    AboutComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
